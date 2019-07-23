89558ceedf44dd44f343661ceeb27d7a5434ff35
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-07-23 12:50:01 +0200

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function doStuff(firstInput, secondInput) {
+    return 'aaahahahahaha';
+}
+doStuff(1, "aaaa");
+;
+var someDumbObj;
+someDumbObj = {
+    name: 'pera',
+    age: 5,
+    asdf: 1
+};
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+function doStuff(firstInput: number, secondInput: string): string {
+    return 'aaahahahahaha';
+}
+
+doStuff(1, "aaaa");
+
+interface thingy  {
+    name: string,
+    age: number
+    asdf?: any
+};
+
+let someDumbObj: thingy;
+someDumbObj = {
+    name: 'pera',
+    age: 5,
+    asdf: 1
+}
+
+export {};
